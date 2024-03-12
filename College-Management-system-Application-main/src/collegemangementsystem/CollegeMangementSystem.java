9718dfd36ce4e7d23bfe787a732740e56a4192ae
FAHAD ALAMOUDI <120297336+FAHADALAMOUDI1@users.noreply.github.com>
2024-03-12 12:47:02 +0300

    Update CollegeMangementSystem.java

--- College-Management-system-Application-main/src/collegemangementsystem/CollegeMangementSystem.java
+++ College-Management-system-Application-main/src/collegemangementsystem/CollegeMangementSystem.java
@@ -4,21 +4,31 @@ import java.sql.*;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author kishor
- */
 public class CollegeMangementSystem {
+    private static CollegeMangementSystem instance;
+    
+    // Private constructor to prevent instantiation from outside the class
+    private CollegeMangementSystem() {
+        // Initialization code, if any
+    }
+    
+    // Static method to get the singleton instance
+    public static CollegeMangementSystem getInstance() {
+        if (instance == null) {
+            instance = new CollegeMangementSystem();
+        }
+        return instance;
+    }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println(" JDBC Start ");
-        
-        System.out.println("Programis running on gui...");
+    public void start() {
+        System.out.println("JDBC Start ");
+        System.out.println("Program is running on GUI...");
         HomePage hp = new HomePage();
         hp.setVisible(true);
     }
 
+    public static void main(String[] args) {
+        CollegeMangementSystem cms = CollegeMangementSystem.getInstance();
+        cms.start();
+    }
 }
